d7beaf459dccdad93cd93f968c2579e9826b9505
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2022-10-30 10:21:38 +0330

    new version of the chapter two (The C programming)

--- chapter_two.c
+++ chapter_two.c
@@ -11,18 +11,18 @@
 int main()
 {
 
-    int year;
-
+    int year , i;
+    char c;
     printf("Enter the year : ... \n");
     scanf("%d", &year);
 
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-    {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
         printf(" %d is a leap year .", year);
     }
     else
         printf("%d is not a leap year", year);
 
+    for (i = 0; ((i < 300 - 1)(c = getchar()) != '\n'))
 
     return 0;
 
