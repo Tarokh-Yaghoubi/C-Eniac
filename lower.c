2eaa46f86c9e56041c333d6f57bae767ed0eaa39
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2022-11-06 10:01:16 +0330

    convert upper case chars to lower case using ASCII character set

---
+++ lower.c
@@ -0,0 +1,23 @@
+//
+// Created by tarokh.yaghoubi on 11/6/2022.
+//
+
+#include <stdio.h>
+
+int lower(int c);
+
+int main()
+{
+
+    lower('N');
+    return 0;
+
+}
+
+int lower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c + 'a' - 'A';
+    else
+        return c;
+}
