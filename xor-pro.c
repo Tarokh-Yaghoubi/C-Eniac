3d72cbef592c3c92374c12ca09cdcb47305f0322
Tarokh Yaghoubi <tarokhgit@gmail.com>
2022-11-23 19:59:29 +0330

    re-wrote the XOR Program

--- xor-pro.c
+++ xor-pro.c
@@ -4,62 +4,38 @@ int xorpro(int arr[], int len);
 
 int main()
 {
+	int p[16] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
 
-	int n[16] = { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-	xorpro(n, 16);
+	xorpro(p, 16);
 
 	return 0;
-
 }
+
+/* XOR two Arrays index by index */
 
 int xorpro(int arr[], int len)
 {
-	int i, ans, x, cnt;
-	int first_segment[8]; 
+	int i, x;
+	int first_segment[8];
 	int second_segment[8];
+	int count;
 
-	int first_index, second_index;
-	
-	cnt = len / 2;
+	count = len / 2;
 
-	for (i = 0; i < cnt; ++i)
-		arr[i];
+	for (i = 0, x = 0; i < 8 && x < 8; ++i, ++x)
+		first_segment[x] = arr[i];
 
-	for (i = 0, x = 0; i < cnt && x < cnt; ++i, ++x)
-			first_segment[x] = arr[i];
+	count = 16;
 
-	
-	for (i = 0; i < cnt; i++)
-	{
-		printf("%d\n", first_segment[i]);
-	}
-	
-
-	printf("-------------------------------\n");
-
-	for (i = 8, x = 0; i < 16 && x < 8; ++i, ++x)
+	for (i = 8, x = 0; i <= count && x < 8; ++i, ++x)
 		second_segment[x] = arr[i];
 
 
-	for (i = 0; i < 8; ++i)
-		printf("%d\n", second_segment[i]);
-
-
-	printf("-------------------------------\n");
-
-	first_index = first_segment[0];
-	second_index = second_segment[0];
-
-	printf(" first segment - first index : %d\t second segment - first index : %d\n", first_index, second_index);
-
-
-	printf("-------------------------------\n");
+	printf("-*-*-*-*-*-*-*--*-*-*-*-\n");
 
 	for (i = 0; i < 8; ++i)
-		printf(" %d", first_segment[i] ^ second_segment[i]);
-	
+		printf("%d\n", first_segment[i] ^ second_segment[i]);
+
 
 	return 0;
-	
 }
