aa89dc2259bc946c7674d1317b51318b6b48acd9
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2022-11-27 16:59:07 +0330

    my first pointer

--- pointers.c
+++ pointers.c
@@ -3,15 +3,20 @@
 int main()
 {
 	int num;
+	int n2 = 1;
 	int* x;
+	int* n;
 
 	printf("Type in a number .. \n");
 
 	scanf("%d", &num);
 
 	x = &num;
+	n = &n2;
+
 
 	printf("The number is : %d\n", *x);
+	printf("Heres a sum : %d", (*x + *n));
 
 	return 0;
 }
