7ccd1596676af8dd73d503d9e1cb5ec6a4c0f80e
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2022-11-28 09:50:37 +0330

    second version of XOR Function

--- myxor.c
+++ myxor.c
@@ -1,34 +1,73 @@
 #include <stdio.h>
 
-int myxor(int arr[], int len);
+/* The XOR progrm : 
+		
+		- a program to split a list and calculate xor of each pair of indexes
+		
+*/
+
+int calxor(int arr[], int);
+
+int lst[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12, 13, 14, 15, 16 };
 
 int main()
 {
+	// extern int lst[];
 
-	int n[5] = {11, 12, 13, 15, 16};
+	int len;
+	int j = 0;
+	int i = 0;
+	int ans = 0;
 
-	myxor(n, 5);
+	for (i = 0, j = 0; i < sizeof(lst); i += sizeof(int), j++) {
+
+
+		printf("%d \n", lst[j]);
+
+	}
 	
+	len = j;
+
+	ans = len % 8;
+	if (ans == 0)
+	{
+		printf(". . . . . . .  . . . . . . . . . . \n");
+		printf("LENGTH MATCH :) \n REMAINDER : .. %d\n", ans);
+		printf("XOR CALCULATION : \n");
+		calxor(lst, len);
+	}
+
+
+	else {
+
+		printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+		printf("! NOT AN APROPRIATE LENGTH ! \n");
+
+	}
+
 	return 0;
 }
 
 
-int myxor(int arr[], int len)
+int calxor(int arr[], int len)
 {
-	int i, ans;
+	int i, x;
+	int first_segment[8], second_segment[8];
 
-	ans = arr[0] ^ arr[1];
+	//for (i = 0; i < len; ++i)
+		//printf("%d\n", arr[i]);
 
-	
-	if (len == 0)
-		printf("%d\n", 0);
-	if (len == 1)
-		printf("%d\n", 1);
+	for (i = 0, x = 0; i < len && x < 8; ++x, ++i)
+		first_segment[x] = arr[i];
 
-	for (i = 2; i < len; ++i)
-		ans = ans ^ arr[i];
+	for (i = 8, x = 0; i < len && x < 8; ++x, ++i)
+		second_segment[x] = arr[i];
 
-	printf("%d\t\n", ans);
+	printf("- - - - - - - - - - - - - - - - - - - \n");
+
+	for (i = 0; i < 8; i++)
+		printf(" %d", (first_segment[i] ^ second_segment[i]));
 
 	return 0;
+
 }
