0f35bc36ff2d1d464e2a8afa12216581cd4f7e07
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2022-12-05 15:56:49 +0330

    tt changed

--- tt.c
+++ tt.c
@@ -32,33 +32,183 @@
 				|WXkl.,kOOO'    .kMMMMMMMMMMMM. ;x.  .'ldl xWW|
 				|o.:dOOOOOOO:     ;MMMMMMMMMMK  oOOOOOOOOOc.NW|
 				|,'OOOOOOOOOOo     XMMMMMMMMMl  OOOOOOOOOOOc.k|
-				|c.OOOOOOOOOOOk,  ;XMMMMMMWKo  ;OOOOOOOOOOOk, |
+				|c.OOOOOOOOOOOk,   ;XMMMMMMWK   ;OOOOOOOOOOOk,|
 				| :OOOOOOOOOOOOOo    .''..     kOOOOOOOOd;',lX|
 				|l.',;:lokOOOOOOO'    ....    .OOOOOOk:.c0WWWW|
 				|WWWNK0kd:,',::;.,0NWWWWWWWWWK.,dxdl'.oNWWWWWW|
 				|WWWWWWWWWWNOdoxNWWWWWWWWWWWWWWl.  ,kWWWWWWWWW| */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+ /*
+int* getrandom();
+
+int main_1()
+{
+	// { a pointer to an int } 
+
+	int* p;
+
+	int i;
+
+	p = getrandom();
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("*(p + %d) : %d \n", i, *(p + i));
+	}
+	return 0;
+
+} 
+
+*/
+/*
+int* getrandom()
+{
+	static int random_array[10];
+	int i;
+
+	srand((unsigned)time(NULL));
+
+	for (i = 0; i < 10; i++)
+	{
+		random_array[i] = rand();
+		printf("random_number [%d] = %d\n", i, random_array[i]);
+
+	}
+
+	return random_array;
+
+} */
+
+\
+						
+// char change_indexes(char* first, char* second, char* last);
+
+// char change_indexes(char* first, char* second, char* last);
+
+const int MAX = 3;
+
+int  repeat(int* arr, int size, int number);
+char match_finder(const char* first, const char* second, int size);
+char match_finder_2(const char* str_1, const char* str_2);
+void replace(int* arr, int size, int from, int to);
+
 int main()
 {
-	/* array with 5 rows and 2 columns */
+	int from, to;
+	int i = 0;
+	char replace_resualt;
+	int arr[10] = { 1, 1, 2, 3, 4, 5, 1, 1, 1, 1 };
+	int resault;
 
-	int arr[5][2] = {
-		
-		{0, 1},
-		{2, 3},
-		{4, 5},
-		{6, 7},
-		{8, 9}
-	};
+	char first[6] = { 'a', 'b', 'c', 'd', 'e', '\0'};
+	char second[6] = { 'o', 'r', 'p', 'z', 'f', '\0'};
 
-	int i, j;
+	char str_1[1024*20];
+	char str_2[1024];
 
-	for (i = 0; i < 5; i++)
-		for (j = 0; j < 2; j++)
-			printf("arr[%d][%d] = %d\n", i, j, arr[i][j]);
+	strcpy(str_1, "The time() function is defined in time.h (ctime in C++) header file. This function returns the time since 00:00:00 UTC, January 1, 1970 (Unix timestamp) in seconds. If second is not a null pointer, the returned value is also stored in the object pointed to by second.");
+	strcpy(str_2, "Unix timestamp");
 
+
+
+
+	printf("Running the function : ... \n ");
+
+	resault = repeat(arr, 10, 1);
+
+	printf("The result is : %d\n", resault);
+
+	printf("-----------------------------------\n");
+
+	printf("From : ... \n");
+	scanf("%d", &from);
+	printf("To: ... \n");
+	scanf("%d", &to);
+
+	//for (i = 0; i < 10; i++)
+		//if (arr[i] == from)
+
+	printf("B "); for (i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) printf("%d ", arr[i]); printf("\n");
+
+	replace(arr, 10, from, to);
+
+	printf("A "); for (i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) printf("%d ", arr[i]); printf("\n");
+
+	printf("------------------------------\n");
+	printf("Match Finder : .. \n");
+	replace_resualt = match_finder(first, second, (sizeof(first) / sizeof(first[0])));
+
+	printf("%d\n", replace_resualt);
+
+
+	replace_resualt = match_finder_2(str_1, str_2);
+
+	printf("%d\n", replace_resualt);
 
 	return 0;
 
+}
+
+int repeat(int* arr, int size, int number)
+{
+	int i = 0;
+	int count = 0;
+
+	for (i = 0; i < size; i++)
+		if (arr[i] == number)
+			count++;
+
+	return count;
+}
+
+void replace(int* arr, int size, int from, int to)
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+		if (arr[i] == from)
+			arr[i] = to;
+
+	/*
+		for (i = 0; i < size; i++)
+			printf("%d ", arr[i]);
+		*/
+
+		//	return arr;
+
+}
+
+char match_finder(const char* first, const char* second, int size)
+{
+	int i, j;
+	i = j = 0;
+
+	for (i = 0, j = 0; i < size && j < size; i++, j++)
+		if (first[i] == second[j])
+		{
+			return 1;
+		}
+		else
+			return 0;
+
+
+}
+
+char match_finder_2(const char* str_1, const char* str_2)
+{
+	int i, j;
+
+	for (i = 0, j = 0; i < strlen(str_1) && j < strlen(str_2); i++, j++)
+		while (str_1[i] != '\0')
+			i++;
+		if (str_1[i] == str_2[j])
+			return 1;
+		else
+			return 0;
 }
